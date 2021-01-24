860c977ce204c2e5539ea11d05c3dc9cc67a5d38
Tristan LaRoche <laroche11235@gmail.com>
2021-01-24 18:58:30 -0500

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -20,8 +20,7 @@ $("window").ready(function () {
 
     for (let i = 0; i < timeArr.length; i++) {
         if (parseInt(currentTime) > i + 9) {
-            timeArr[i].find("textarea").addClass('past')
-            timeArr[i].find("textarea", "button").attr("disabled", "disabled")
+            timeArr[i].find("textarea").addClass('past').attr("disabled", "disabled")
         } else if (parseInt(currentTime) == i + 9) {
             timeArr[i].find("textarea").addClass('present')
         } else {
